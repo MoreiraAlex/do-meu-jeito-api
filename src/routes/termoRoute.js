b6c18d401e3136f04ccaa4c2fb304b3028751c85
Alex Moreira <amoreira@allianceconsultoria.com.br>
2025-06-09 13:51:54 -0300

    Adiciona autenticação ao acesso das rotas de jogos

--- src/routes/termoRoute.js
+++ src/routes/termoRoute.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const {requireAuth } = require("@clerk/express");
 const { listGames, listGamesByUser, listGamesByVisibilty, listGameById, createGame, updateGame, deleteGameById, checkPasswordGame } = require("../controllers/termoController");
 
 const router = express.Router();
 
 router.get("/", listGames);
 router.get("/page", listGamesByVisibilty);
-router.get("/page/:userId", listGamesByUser);
-router.get("/:id", listGameById);
-router.post("/", createGame);
-router.put("/:id", updateGame);
-router.delete("/:id", deleteGameById);
+router.get("/page/:userId", requireAuth(), listGamesByUser);
+router.get("/:id", requireAuth(), listGameById);
+router.post("/", requireAuth(), createGame);
+router.put("/:id", requireAuth(), updateGame);
+router.delete("/:id", requireAuth(), deleteGameById);
 router.post("/check-password", checkPasswordGame);
 
 module.exports = router;
